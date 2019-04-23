963af3a3a0a14a0e0614ea9ad9df0d384583c0f9
mwu233 <40613916+mwu233@users.noreply.github.com>
2019-04-23 13:23:36 -0500

    updated

--- js/main.js
+++ js/main.js
@@ -22,12 +22,17 @@ function createMap(){
     
     curMap = map;
     
+    // add basemap tilelayer
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    }).addTo(curMap);
+    
     //add OSM base tilelayer
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    /*L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
         subdomains: 'abcd',
         maxZoom: 19
-    }).addTo(curMap);
+    }).addTo(curMap);*/
 
     //call getData function
     getData(curMap);
