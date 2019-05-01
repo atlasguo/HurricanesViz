135eb81c7e05beb3998d1a25ac3cb489048c7b71
BB <vongkusolkit@wisc.edu>
2019-04-30 23:05:27 -0500

    reset graph to match workflow

--- js/main.js
+++ js/main.js
@@ -766,7 +766,11 @@ function applySetting() {
 
 
                                             });
+                                            // console.log("HERE")
+
                                             createScatter(graphData);
+                                            document.getElementById("lineGraph-div").innerHTML = "";
+
 
                                         } // end of scenario #3 if location is found and resulting hurricane # > 0
 
@@ -1435,7 +1439,7 @@ function createLineGraph(data) {
 
     // Add X axis --> it is a date format
     var x = d3.scaleLinear()
-    .domain([xmin - xmin / 1200, xmax + xmax / 1200])
+    .domain([xmin - xmin / 1000, xmax + xmax / 1000])
     .range([0, width]);
 
 
